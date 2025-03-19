673117034a266cff2e3cd8481fc92aefd38da043
archit-dhupar <archit.dhupar@gmail.com>
2025-03-19 22:32:31 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,27 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "MediaTailorSDK",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v14), .tvOS(.v14)],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MediaTailorSDK",
-            targets: ["MediaTailorSDK"]
-        )
+            targets: ["MediaTailorSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-      
+
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "MediaTailorSDK",
-            url: "https://github.com/archit-dhupar/DzBase/releases/download/0.0.1/MediaTailorSDK.xcframework.zip",
+            url: "https://github.com/archit-dhupar/MT-SPM/releases/download/0.0.1/MediaTailorSDK.xcframework.zip",
             checksum: "5f78c865ef2627a4143f08cdc40e2af6c9d98bbe968b5f5d54a69c50e4cdd247")
     ],
     swiftLanguageVersions: [.v5]
 )
-
-
