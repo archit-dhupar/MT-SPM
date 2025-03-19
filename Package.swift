b0dccfc66ff470fb991f7c84247c028dd0e20997
archit-dhupar <archit.dhupar@gmail.com>
2025-03-19 22:09:31 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MediaTailorSDK",
-            targets: ["MediaTailorSDK"]),
+            name: "MediaTailor",
+            targets: ["MediaTailor"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
