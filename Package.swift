d9a9a400610fd6b0219a243a4c3694ed1f10495c
archit-dhupar <archit.dhupar@gmail.com>
2025-03-24 21:36:42 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "MediaTailorSDK",
-            url: "https://github.com/archit-dhupar/MT-SPM/releases/download/0.0.3/MediaTailorSDK.xcframework.zip",
+            url: "https://github.com/archit-dhupar/MT-SPM/releases/download/0.0.6/MediaTailorSDK.xcframework.zip",
             checksum: "5f78c865ef2627a4143f08cdc40e2af6c9d98bbe968b5f5d54a69c50e4cdd247")
     ],
     swiftLanguageVersions: [.v5]
